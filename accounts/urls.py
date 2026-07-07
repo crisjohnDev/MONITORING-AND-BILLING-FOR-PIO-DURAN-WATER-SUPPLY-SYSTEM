1a41b87c45879643175fe5901d81f0f6770c3755
crisjohn.dev.official@gmail.com <crisjohn.dev.official@gmail.com>
2026-07-08 07:15:27 +0800

    update logout view

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('user/logout/', views.logout_view, name='logout'),
 ]
